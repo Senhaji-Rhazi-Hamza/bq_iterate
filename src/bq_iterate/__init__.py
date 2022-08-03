8d63abf6a3e1956f4bef216ce7daa41fedd2fd88
Senhaji-Rhazi-Hamza <hamza.senhajirhazi@gmail.com>
2022-08-03 23:05:22 +0200

    update pyproject.toml

--- src/bq_iterate/__init__.py
+++ src/bq_iterate/__init__.py
@@ -1,6 +1,6 @@
 from bq_iterate.core import BqQueryRowIterator, BqTableRowIterator
 from bq_iterate.utils import batchify_iterator
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 __all__ = ["BqQueryRowIterator", "BqTableRowIterator", "batchify_iterator"]
